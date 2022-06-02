0e0f6f075424bf5bd268c2480e727de594f49dd7
EC2 Default User <ec2-user@ip-172-31-1-26.ap-northeast-1.compute.internal>
2022-06-02 02:05:45 +0000

    [add]4-16 user/edit

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   
 end
